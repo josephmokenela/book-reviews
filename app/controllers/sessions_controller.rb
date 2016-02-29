1934d509df1ff41e8b3b13bd10e3ba45fefa18e7
Joseph Mokenela <joseph.mokenela@gmail.com>
2016-02-29 23:31:06 +0200

    Added in the login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,22 @@
 class SessionsController < ApplicationController
   def new
+  	# Login page - new.html.erb
   end
 
   def create
+  	reviewer = Reviewer.find_by(name: params[:reviewer][:name])
+  	password = params[:reviewer][:password]
+
+  	if reviewer && reviewer.authenticate(password)
+  		session[:reviewer_id] = reviewer.id
+  		redirect_to root_path, notice: "Logged in successfully"
+  	else
+  		redirect_to login_path, alert: "Invalid username/password"
+  	end
   end
 
   def destroy
+  	reset_session # Wipe out the session and everything in it
+  	redirect_to login_path, notice: "You have been logged out"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
 
   resources :sessions, only: [:new, :create, :destroy]
 
+  get "/login" => "sessions#new", as: "login"
+  delete "/logout" => "sessions#destroy", as: "logout"
+
   root to: 'books#index'
 end
